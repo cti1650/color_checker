684b481622bb103981a6768c284f66d3a9879050
cti1650 <cti1650@gmail.com>
2021-03-01 23:02:12 +0900

    update color_picker

--- js/hex2rgb.js
+++ js/hex2rgb.js
@@ -1,6 +1,57 @@
+function rgb2hsv(r, g, b) {
+  // 引数処理
+  let tmp = [r, g, b];
+  if (r !== undefined && g === undefined) {
+    const cc = parseInt(
+      r
+        .toString()
+        .replace(/[^\da-f]/gi, "")
+        .replace(/^(.)(.)(.)$/, "$1$1$2$2$3$3"),
+      16
+    );
+    tmp = [(cc >> 16) & 0xff, (cc >> 8) & 0xff, cc & 0xff];
+  } else {
+    for (let i in tmp)
+      tmp[i] = tmp[i] > 255 ? 255 : tmp[i] < 0 ? 0 : Math.floor(tmp[i]);
+  }
+  r = tmp[0];
+  g = tmp[1];
+  b = tmp[2];
+
+  // RGB to HSV 変換
+  const v = Math.max(r, g, b),
+    d = v - Math.min(r, g, b),
+    s = v ? d / v : 0,
+    h = s
+      ? (v === r
+          ? (g - b) / d + (g < b ? 6 : 0)
+          : v === g
+          ? 2 + (b - r) / d
+          : 4 + (r - g) / d) * 60
+      : 0;
+
+  // 戻り値
+  return {
+    h: h,
+    s: s,
+    v: v / 255,
+  };
+}
+
 // HEXをRGBに変換する方法 => https://lab.syncer.jp/Web/JavaScript/Snippet/61/
 const hex2rgb = (hex) => {
-  const arr = { input: hex, r: null, g: null, b: null, arr: [] };
+  const arr = {
+    input: hex,
+    r: null,
+    g: null,
+    b: null,
+    h: null,
+    s: null,
+    v: null,
+    color_analysis: null,
+    rgb: [],
+    hsv: [],
+  };
   if (hex.slice(0, 1) == "#") hex = hex.slice(1);
   if (hex.length == 3)
     hex =
@@ -18,7 +69,25 @@ const hex2rgb = (hex) => {
   arr["r"] = hexArr[0];
   arr["g"] = hexArr[1];
   arr["b"] = hexArr[2];
-  arr["arr"] = hexArr;
+  arr["rgb"] = hexArr;
+  const hsvArr = rgb2hsv(hexArr[0], hexArr[1], hexArr[2]);
+  arr["h"] = hsvArr["h"];
+  arr["s"] = hsvArr["s"];
+  arr["v"] = hsvArr["v"];
+  if (arr["v"] >= 0.5) {
+    if (arr["s"] >= 0.5) {
+      arr["color_analysis"] = "spring";
+    } else {
+      arr["color_analysis"] = "summer";
+    }
+  } else {
+    if (arr["s"] >= 0.5) {
+      arr["color_analysis"] = "winter";
+    } else {
+      arr["color_analysis"] = "autumn";
+    }
+  }
+  arr["hsv"] = [hsvArr["h"], hsvArr["s"], hsvArr["v"]];
   return arr;
 };
 
